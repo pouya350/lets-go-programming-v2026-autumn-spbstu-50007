17a8ddae3cfc0e34531fd89e740f0a31f8bc48bf
pouya350 <pravyparen1@mail.ru>
2026-09-14 22:28:59 +0300

    [TASK-1] divide add

--- erofey.polozhuk/task-1/cmd/calculator/main.go
+++ erofey.polozhuk/task-1/cmd/calculator/main.go
@@ -1,10 +1,17 @@
 package main
 
-import ("fmt"
-
-
+import (
+	"fmt"
+	"strconv"
 )
 
+func Divide(a, b int) (int, error) {
+	if b != 0 {
+		return a / b, nil
+	}
+	return 0, fmt.Errorf("Division by zero")
+
 func main() {
-	fmt.Print("Hello Commit")
+	
+	
 }
