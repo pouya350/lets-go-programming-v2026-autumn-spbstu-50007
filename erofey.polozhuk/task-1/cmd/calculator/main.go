e32d6a4f3fcf675ea61a699fa0eac5ec3fcaba9e
pouya350 <pravyparen1@mail.ru>
2026-09-14 22:41:11 +0300

    [TASK-1] operation switch case idea

--- erofey.polozhuk/task-1/cmd/calculator/main.go
+++ erofey.polozhuk/task-1/cmd/calculator/main.go
@@ -20,6 +20,26 @@ func Diff(a, b int) (int, error) {
 func Multip(a, b int) (int, error) {
 	return a * b, nil
 }
+func Operation(a ,b int ,opertion string)(int, error){
+	var res int
+	var err error
+	switch opertion{
+	case "+":
+		res, err = Summa(a,b)
+		return res, err
+	case "-":
+		res, err = Diff(a,b)
+		return res, err
+	case "*":
+		res, err = Multip(a,b)
+		return res, err
+	case "/":
+		res,err = Divide(a,b)
+		return res, err
+	default:
+		return 0, fmt.Errorf("Invalid operation")
+	}
+}
 
 func main() {
 	
