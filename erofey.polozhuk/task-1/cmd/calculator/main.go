9ec8e3fff55e4376c7d8093ffee4edc3bb38bc26
pouya350 <pravyparen1@mail.ru>
2026-09-14 22:29:37 +0300

    [TASK-1] new oper summ  add

--- erofey.polozhuk/task-1/cmd/calculator/main.go
+++ erofey.polozhuk/task-1/cmd/calculator/main.go
@@ -10,6 +10,10 @@ func Divide(a, b int) (int, error) {
 		return a / b, nil
 	}
 	return 0, fmt.Errorf("Division by zero")
+}
+func Summa(a, b int) (int, error) {
+	return a + b, nil
+}
 
 func main() {
 	
