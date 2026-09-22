16dfe259538c778a76ae8e8bfd341f9b5ddc7acf
pouya350 <pravyparen1@mail.ru>
2026-09-22 15:29:41 +0300

    [TASK-1] error return delete n variables only in switch case

--- erofey.polozhuk/task-1/cmd/service/main.go
+++ erofey.polozhuk/task-1/cmd/service/main.go
@@ -10,43 +10,41 @@ func Divide(a, b int) (int, error) {
 	}
 	return 0, fmt.Errorf("Division by zero")
 }
-func Summa(a, b int) (int, error) {
-	return a + b, nil
+func Summa(a, b int) int {
+	return a + b
 }
-func Diff(a, b int) (int, error) {
-	return a - b, nil
+func Diff(a, b int) int {
+	return a - b
 }
-func Multip(a, b int) (int, error) {
-	return a * b, nil
+func Multip(a, b int) int {
+	return a * b
 }
 func Operation(a, b int, opertion string) (int, error) {
-	var res int
-	var err error
+
 	switch opertion {
 	case "+":
-		res, err = Summa(a, b)
-		return res, err
+		res := Summa(a, b)
+		return res, nil
 	case "-":
-		res, err = Diff(a, b)
-		return res, err
+		res := Diff(a, b)
+		return res, nil
 	case "*":
-		res, err = Multip(a, b)
-		return res, err
+		res := Multip(a, b)
+		return res, nil
 	case "/":
-		res, err = Divide(a, b)
+		res, err := Divide(a, b)
 		return res, err
 	default:
 		return 0, fmt.Errorf("Invalid operation")
 	}
 }
 
-var (
-	a         int
-	b         int
-	operation string
-)
-
 func main() {
+	var (
+		a         int
+		b         int
+		operation string
+	)
 	_, err := fmt.Scan(&a)
 	if err != nil {
 		fmt.Println("Invalid first operand")
